adf72e0f7e934a43690aa27b5acf01c0b3455f40
yeowinzer <yeowinzer@gmail.com>
2022-04-24 01:47:57 +0800

    yes
    yes

--- public/js/search.js
+++
@@ -1,6 +0,0 @@
-const searchKey = decodeURI(location.pathname.split('/').pop());
-
-const searchSpanElement = document.querySelector('#search-key');
-searchSpanElement.innerHTML = searchKey;
-
-getProducts(searchKey).then(data => createProductCards(data, '.card-container'));
